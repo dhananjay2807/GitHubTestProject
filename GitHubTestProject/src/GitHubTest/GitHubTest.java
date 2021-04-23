93ad40a46417cd67748d0693c466dc83526e9d06
Dhananjay <dhananjay.yadav@neosoftmail.com>
2021-04-23 11:29:01 +0530

    First commit

---
+++ GitHubTestProject/src/GitHubTest/GitHubTest.java
@@ -0,0 +1,12 @@
+package GitHubTest;
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		String code = "SE";
+		if(code == "SE")
+			System.out.println("software engineer");
+		else
+			System.out.println("not a software engineer");
+	}
+}
